73117cab5685d3d8672a6921b6728d953846aa06
Geri Borbás <geri@eppz.eu>
2022-03-30 01:47:02 +0200

    0.0.1

--- Package.swift
+++ Package.swift
@@ -4,25 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUI_Pull_to_Refresh",
+    name: "Refreshable",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUI_Pull_to_Refresh",
-            targets: ["SwiftUI_Pull_to_Refresh"]),
+            name: "Refreshable",
+            targets: ["Refreshable"]
+		)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+		.package(
+			url: "https://github.com/siteline/SwiftUI-Introspect.git",
+			.branch("master")
+		)
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftUI_Pull_to_Refresh",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftUI_Pull_to_RefreshTests",
-            dependencies: ["SwiftUI_Pull_to_Refresh"]),
+            name: "Refreshable",
+            dependencies: []
+		)
     ]
 )
--- Sources/Refreshable/Refreshable.swift
+++ Sources/Refreshable/Refreshable.swift
@@ -1,6 +1,5 @@
 public struct SwiftUI_Pull_to_Refresh {
-    public private(set) var text = "Hello, World!"
-
+	
     public init() {
     }
 }
